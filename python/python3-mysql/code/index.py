53fe6f2ef4f7223b6df63f2973996fa0b6ed756a
penghuima <2992426202@qq.com>
2022-07-19 20:26:21 +0800

    refine python3 mysql example
    Signed-off-by: penghuima <2992426202@qq.com>

--- python/python3-mysql/code/index.py
+++ python/python3-mysql/code/index.py
@@ -9,21 +9,29 @@ logger = logging.getLogger()
 connection = None
 
 # initialize hook, connect to the database
+
+
 def initialize(context):
     global connection
     try:
         connection = pymysql.connect(
-            host=os.environ['MYSQL_ENDPOING'],   # 替换为您的HOST名称。
-            port=int(os.environ['MYSQL_PORT']),  # 替换为您的端口号。
-            user=os.environ['MYSQL_USER'],       # 替换为您的用户名。
-            passwd=os.environ['MYSQL_PASSWORD'], # 替换为您的用户名对应的密码。
-            db=os.environ['MYSQL_DBNAME'],       # 替换为您的数据库名称。
+            # Replace it with your host name.
+            host=os.environ['MYSQL_ENDPOING'],
+            # Replace it with your port number.
+            port=int(os.environ['MYSQL_PORT']),
+            # Replace it with your username.
+            user=os.environ['MYSQL_USER'],
+            # Replace the  password with the one corresponding to your username.
+            passwd=os.environ['MYSQL_PASSWORD'],
+            # Replace it with the name of your database.
+            db=os.environ['MYSQL_DBNAME'],
             connect_timeout=5)
     except Exception as e:
         logger.error(e)
         logger.error(
             "ERROR: Unexpected error: Could not connect to MySql instance.")
         raise Exception(str(e))
+
 
 def pre_stop(context):
     logger.info("pre_stop hook start.")
@@ -34,14 +42,23 @@ def pre_stop(context):
 def handler(event, context):
     if connection is None:
         raise Exception("Mysql connection not initialized.")
-    
+
     # Check if the server is alive.
     # If the connection is closed, reconnect.
     connection.ping(reconnect=True)
-
     try:
+        # Database write operation
         with connection.cursor() as cursor:
-            cursor.execute("SELECT * FROM users")
+            # the primary key id is self-incrementing
+            sql = "INSERT INTO `USERS` (`NAME`, `AGE`) VALUES (%s, %s)"
+            cursor.execute(sql, ("王二", 38))
+            # connection is not autocommit by default. So you must commit to save your changes.
+            connection.commit()
+            logger.info(
+                "Successfully insert a piece of data into the database")
+        # Database read operation
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM USERS ORDER BY ID DESC")
             result = cursor.fetchone()
             logger.info(result)
             return "user: {}".format(result)
@@ -49,5 +66,3 @@ def handler(event, context):
         logger.error(
             "ERROR: Unexpected error: Could not connect to MySql instance.")
         raise Exception(str(e))
-
-
