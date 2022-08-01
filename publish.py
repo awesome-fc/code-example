44e0f4357642bb6f019a53e09f62000957d896b8
lowkeyrd <wangrenda1987@gmail.com>
2022-08-01 16:58:02 +0800

    Update publish.py

--- publish.py
+++ publish.py
@@ -48,30 +48,31 @@ with open('update.list') as f:
 for eve_app in publish_list:
     times = 1
     os.chdir(workspace)
-#     while times <= 3:
-#         print("----------------------: ", eve_app)
-#         # publish app to registry 
-#         publish_script = 'https://serverless-registry.oss-cn-hangzhou.aliyuncs.com/publish-file/python3/hub-publish.py'
-#         command = 'cd %s && wget %s && python hub-publish.py' % (
-#             eve_app, publish_script)
-#         child = subprocess.Popen(
-#             command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, )
-#         stdout, stderr = child.communicate()
-#         if child.returncode == 0:
-#             print("stdout:", stdout.decode("utf-8"))
-#             break
-#         else:
-#             print("stdout:", stdout.decode("utf-8"))
-#             print("stderr:", stderr.decode("utf-8"))
-#             time.sleep(3)
-#             if times == 3:
-#                 raise ChildProcessError(stderr)
-#         times = times + 1
+    while times <= 3:
+        print("----------------------: ", eve_app)
+        # publish app to registry 
+        publish_script = 'https://serverless-registry.oss-cn-hangzhou.aliyuncs.com/publish-file/python3/hub-publish.py'
+        command = 'cd %s && wget %s && python hub-publish.py' % (
+            eve_app, publish_script)
+        child = subprocess.Popen(
+            command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, )
+        stdout, stderr = child.communicate()
+        if child.returncode == 0:
+            print("stdout:", stdout.decode("utf-8"))
+            break
+        else:
+            print("stdout:", stdout.decode("utf-8"))
+            print("stderr:", stderr.decode("utf-8"))
+            time.sleep(3)
+            if times == 3:
+                raise ChildProcessError(stderr)
+        times = times + 1
 
     # publish code.zip to oss
+    os.chdir(workspace)
     makefile = Path('%s/src/code/Makefile' % (eve_app))
-    print(makefile)
     if makefile.is_file():
+        print("----------------------Makefile: ", makefile)
         command = 'cd %s/src/code && make release' % (eve_app)
         child = subprocess.Popen(
             command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, )
