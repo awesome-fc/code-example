8a826964b344413ca4d1b0ea595ca22c2b73c0d8
封崇 <renda.wrd@alibaba-inc.com>
2022-08-09 17:45:50 +0800

    optimize publish steps

--- publish.py
+++ publish.py
@@ -51,42 +51,55 @@ workspace = os.getcwd()
 with open('update.list') as f:
     publish_list = [eve_app.strip() for eve_app in f.readlines()]
 
+failed_registry = []
+failed_oss = []
 for eve_app in publish_list:
     times = 1
     os.chdir(workspace)
-#     while times <= 3:
-#         print("----------------------: ", eve_app)
-#         # publish app to registry 
-#         publish_script = 'https://serverless-registry.oss-cn-hangzhou.aliyuncs.com/publish-file/python3/hub-publish.py'
-#         command = 'cd %s && wget %s && python hub-publish.py' % (
-#             eve_app, publish_script)
-#         child = subprocess.Popen(
-#             command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, )
-#         stdout, stderr = child.communicate()
-#         if child.returncode == 0:
-#             print("stdout:", stdout.decode("utf-8"))
-#             break
-#         else:
-#             print("stdout:", stdout.decode("utf-8"))
-#             print("stderr:", stderr.decode("utf-8"))
-#             time.sleep(3)
-#             if times == 3:
-#                 raise ChildProcessError(stderr)
-#         times = times + 1
+    try:
+        while times <= 3:
+            print("----------------------: ", eve_app)
+            # publish app to registry 
+            publish_script = 'https://serverless-registry.oss-cn-hangzhou.aliyuncs.com/publish-file/python3/hub-publish.py'
+            command = 'cd %s && wget %s && python hub-publish.py' % (
+                eve_app, publish_script)
+            child = subprocess.Popen(
+                command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, )
+            stdout, stderr = child.communicate()
+            if child.returncode == 0:
+                print("stdout:", stdout.decode("utf-8"))
+                break
+            else:
+                print("stdout:", stdout.decode("utf-8"))
+                print("stderr:", stderr.decode("utf-8"))
+                time.sleep(3)
+                if times == 3:
+                    raise ChildProcessError(stderr)
+            times = times + 1
+    except Exception as e:
+        print('Failed to publish registry, app %s, err: %s' % (eve_app, e))
+        failed_registry.append(eve_app)
 
     # publish code.zip to oss
     os.chdir(workspace)
-    makefile = Path('%s/src/code/Makefile' % (eve_app))
-    if makefile.is_file():
-        print("----------------------Makefile: ", makefile)
-        command = 'cd %s/src/code && make release' % (eve_app)
-        print(command)
-        child = subprocess.Popen(
-            command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, )
-        stdout, stderr = child.communicate()
-        print("stdout:", stdout.decode("utf-8"))
-        if child.returncode != 0:
-            print("stderr:", stderr.decode("utf-8"))
-            raise ChildProcessError(stderr)
-    code_zip = zip_file(workspace, eve_app)
-    upload_oss(eve_app, code_zip)
+    try:
+        makefile = Path('%s/src/code/Makefile' % (eve_app))
+        if makefile.is_file():
+            print("----------------------Makefile: ", makefile)
+            command = 'cd %s/src/code && make release' % (eve_app)
+            print(command)
+            child = subprocess.Popen(
+                command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, )
+            stdout, stderr = child.communicate()
+            print("stdout:", stdout.decode("utf-8"))
+            if child.returncode != 0:
+                print("stderr:", stderr.decode("utf-8"))
+                raise ChildProcessError(stderr)
+        code_zip = zip_file(workspace, eve_app)
+        upload_oss(eve_app, code_zip)
+    except Exception as e:
+        print('Failed to publish oss, app %s, err: %s' % (eve_app, e)) 
+        failed_oss.append(eve_app)
+    
+print('Failed registry list: ', failed_registry)
+print('Failed oss list: ', failed_oss)
