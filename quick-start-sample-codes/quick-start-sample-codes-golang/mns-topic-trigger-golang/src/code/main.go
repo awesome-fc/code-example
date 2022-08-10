4cfea09d73de9f52ff7863d7d1105e39421b6560
penghuima <2992426202@qq.com>
2022-08-10 16:31:07 +0800

    refine mns topic trigger
    Signed-off-by: penghuima <2992426202@qq.com>

--- quick-start-sample-codes/quick-start-sample-codes-golang/mns-topic-trigger-golang/src/code/main.go
+++ quick-start-sample-codes/quick-start-sample-codes-golang/mns-topic-trigger-golang/src/code/main.go
@@ -13,19 +13,19 @@ func HandleRequest(ctx context.Context, event interface{}) (string, error) {
 	fctx, _ := fccontext.FromContext(ctx)
 	flog := fctx.GetLogger()
 	var topicEvent events.MnsTopicEvent
+	flog.Info("Receive mns topic whole message:", event)
 	switch mess := event.(type) {
+	// the format of event is STREAM
 	case string:
 		{
-			flog.Info("event:", event)
-			return fmt.Sprintf("MessageBody:%s", mess), nil
+			return fmt.Sprintf("the event format is STREAM and message is:%s", mess), nil
 		}
+	// the format of event is JSON
 	default:
 		result, _ := json.Marshal(mess)
 		_ = json.Unmarshal(result, &topicEvent)
-		flog.Info("event:", event)
-		return fmt.Sprintf("MessageBody:%s", *topicEvent.Message), nil
+		return fmt.Sprintf("the event format is JSON and MessageBody is:%s", *topicEvent.Message), nil
 	}
-
 }
 
 func main() {
--- quick-start-sample-codes/quick-start-sample-codes-python/mns-topic-trigger-python/src/code/index.py
+++ quick-start-sample-codes/quick-start-sample-codes-python/mns-topic-trigger-python/src/code/index.py
@@ -1,7 +1,16 @@
 import logging
+import json
 
 
 def handler(event, context):
     logger = logging.getLogger()
-    logger.info(event)
-    return "mns_topic trigger event = {}".format(event)
+    logger.info('Receive mns topic whole message:{}'.format(event))
+    # Parse the json
+    try:
+        eventObect = json.loads(event)
+    except Exception:
+        return "the event format is STREAM and mns topic message content is:{}".format(event)
+    else:
+        if 'Message' in eventObect:
+            return "the event format is JSON and mns topic message content is:{}".format(eventObect['Message'])
+        return "the event format is STREAM and mns topic message content is:{}".format(event)
